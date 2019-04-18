a2589d60cdda5e4be3388f64a307049280bae1aa
Anthony DiRusso <ossurid@gmail.com>
2019-04-18 11:58:59 -0400

    adds index.js scripts

--- scripts/index.js
+++ scripts/index.js
@@ -34,7 +34,106 @@ $(window).scroll(function() {
 
     if (scroll >= about - navHeight) {
         $('.menu li').removeClass('selected');
-        
+        $('about_button li').addClass('selected');
+    }
+    if (scroll >= portfolio - navHeight) {
+        $('.menu li').removeClass('selected');
+        $('.portfolio_button li').addClass('selected');
+    }
+    //If at the top of the page, remvoe all selected classes
+    if (scroll === 0) {
+        $('.menu li').removeClass('selected');
+    }
+    //If at bottom of page, add selected class on Contact
+    if (scroll + windowHeight === documentHeight) {
+        $('.menu li').removeClass('selected');
+        $('.contact_button li').addClass('selected');
     }
 
-})
+});
+
+
+//Contact image button animation
+$('.contact img').mouseover(function() {
+    $(this).animate({
+        padding: '0',
+    }, 200);
+});
+$('.contact img').mouseout(function() {
+    $(this).animate({
+        padding: '5px',
+    }, 200);
+});
+
+//Add sticky navbar on scroll
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){
+        $('.nav').addClass("sticky");
+    } else {
+        $('.nav').removeClass("sticky");
+    }
+});
+
+//Show and hide hamburger menu
+$('.hamburger-icon').click(function() {
+    $('.hamburger-menu').toggle();
+});
+
+//If on touch, toggle overlya when project is clicked
+if(Modernizr.touchevents) {
+    //When image is clicked on touch devic add hover class(show overlay)
+    $('.project img').on('click', function(event){
+        event.preventDefautl();
+        $(this).parent('.project').addClass('hover');
+    });
+    //When overlay is clicked on touch device, remove hover class(hide overlay)
+    $('.project .overlay').on('click', function() {
+        $(this).parent('.project').removeClass('hover');
+    });
+}
+
+//transistion to different parts of page 
+$(".contact_button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1500);
+    $('.menu li').removeClass('selected');
+    $(this).children().addClass('selected');
+    let windowSize = $(window).width();
+    if (windowSize <= 540) {
+        $('.hamburger-menu').toggle();
+    }
+});
+
+$(".about_button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 1500);
+    $('.menu li').removeClass('selected');
+    $(this).children().addClass('selected');
+    let windowSize = $(window).width();
+    if (windowSize <= 540) {
+        $('.hamburger-menu').toggle();
+    }
+});
+
+$(".portfolio_button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 1500);
+    $('.menu li').removeClass('selected');
+    $(this).children().addClass('selected');
+
+    let windowSize = $(window).width();
+    if (windowSize <= 540) {
+        $('.hamburger-menu').toggle();
+    }
+});
+
+$(".logo").click(function() {
+    $('.menu li').removeClass('selected');
+    $(this).children().addClass('selected');
+    $('html, body').animate({
+        scrollTop: $("#logo").offset().top
+    }, 1500);
+});
